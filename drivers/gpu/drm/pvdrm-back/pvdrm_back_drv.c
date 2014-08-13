5905f0cb447aa868025722d159c6743e0ebbfb5a
Yusuke Suzuki <utatane.tea@gmail.com>
2014-08-13 10:45:25 +0900

    Add initial stub for pvdrm-back

---
+++ drivers/gpu/drm/pvdrm-back/pvdrm_back_drv.c
@@ -0,0 +1,152 @@
+/*
+  Copyright (C) 2014 Yusuke Suzuki <utatane.tea@gmail.com>
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <linux/console.h>
+#include <linux/device.h>
+#include <linux/module.h>
+
+#include <xen/xen.h>
+#include <xen/xenbus.h>
+#include <xen/events.h>
+#include <xen/page.h>
+#include <xen/platform_pci.h>
+#include <xen/grant_table.h>
+
+#include <xen/interface/memory.h>
+#include <xen/interface/grant_table.h>
+#include <xen/interface/io/protocols.h>
+
+#include <asm/xen/hypervisor.h>
+
+struct pvdrm_back_device {
+	struct xenbus_device* xbdev;
+};
+
+static int pvdrm_back_probe(struct xenbus_device *xbdev, const struct xenbus_device_id *id)
+{
+	int ret;
+	struct pvdrm_back_device* info;
+
+	info = kzalloc(sizeof(struct pvdrm_back_device), GFP_KERNEL);
+	if (!info) {
+		return -ENOMEM;
+	}
+	info->xbdev = xbdev;
+	dev_set_drvdata(&xbdev->dev, info);
+
+	ret = xenbus_switch_state(xbdev, XenbusStateInitWait);
+	if (ret) {
+		return ret;
+	}
+	return 0;
+}
+
+static int pvdrm_back_remove(struct xenbus_device *xbdev)
+{
+	return 0;
+}
+
+static void frontend_changed(struct xenbus_device *xbdev, enum xenbus_state frontend_state)
+{
+	struct pvdrm_back_device* info;
+	int ret = 0;
+	info = dev_get_drvdata(&xbdev->dev);
+
+	printk(KERN_INFO "%s", xenbus_strstate(frontend_state));
+
+	switch (frontend_state) {
+	case XenbusStateInitialising:
+		if (xbdev->state == XenbusStateClosed) {
+			xenbus_switch_state(xbdev, XenbusStateInitWait);
+		}
+		break;
+
+	case XenbusStateInitialised:
+	case XenbusStateConnected:
+		if (xbdev->state == XenbusStateConnected)
+			break;
+
+		/* OK, connect it. */
+		/* TODO: Implement it */
+		break;
+
+	case XenbusStateClosing:
+		xenbus_switch_state(xbdev, XenbusStateClosing);
+		break;
+
+	case XenbusStateClosed:
+		/* TODO: Implement it */
+
+		/* fall through if not online */
+	case XenbusStateUnknown:
+		/* implies xen_blkif_disconnect() via xen_blkbk_remove() */
+		device_unregister(&xbdev->dev);
+		break;
+
+	default:
+		xenbus_dev_fatal(xbdev, -EINVAL, "saw state %d at frontend", frontend_state);
+		break;
+	}
+}
+
+static const struct xenbus_device_id pvdrm_back_ids[] = {
+	{ "vdrm" },
+	{ "" }
+};
+
+static DEFINE_XENBUS_DRIVER(pvdrm_back, ,
+	.probe = pvdrm_back_probe,
+	.remove = pvdrm_back_remove,
+	.otherend_changed = frontend_changed
+);
+
+static int __init pvdrm_back_init(void)
+{
+	int i;
+	int threads;
+
+	if (!xen_domain())
+		return -ENODEV;
+
+	threads = num_online_cpus();
+
+	for (i = 0; i < threads; ++i) {
+	}
+
+	printk(KERN_INFO "Initialising PVDRM backend driver.\n");
+
+	return xenbus_register_backend(&pvdrm_back_driver);
+}
+module_init(pvdrm_back_init);
+
+static void __exit pvdrm_back_exit(void)
+{
+	xenbus_unregister_driver(&pvdrm_back_driver);
+}
+module_exit(pvdrm_back_exit);
+
+MODULE_AUTHOR("Yusuke Suzuki");
+MODULE_DESCRIPTION("PVDRM backend driver");
+MODULE_LICENSE("GPL");
+/* vim: set sw=8 ts=8 et tw=80 : */
